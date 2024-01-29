9e3c1fa01bc6419e92bace638bdf2261986b813e
Aileena <aile0074@stud.kea.dk>
2024-01-29 14:43:40 +0100

    create functions in JS

--- script.js
+++ script.js
@@ -2,30 +2,57 @@
 
 window.addEventListener("load", start);
 
+let number;
+
 function start() {
     console.log("JavaScript is running");
+    number = generateRandomNumber();
+    document.querySelector("#guess-form").addEventListener("submit", receiveGuess);
 }
 
 function generateRandomNumber(){
-
+    return 42;
 }
 
-function receiveGuess(){
+function receiveGuess(event){
+    event.preventDefault();
 
+    const form = event.target;
+    const value = form.guess.valueAsNumber;
+
+    console.log("received guess");
+    console.log(form);
+    console.log(value);
+    checkGuess(value);
 }
 
-function checkGuess(){
-
+function checkGuess(guess){
+    if(guess === number){
+        guessIsCorrect(guess);
+    } else if(guess < number){
+        guessIsTooLow(guess);
+    } else {
+        guessIsTooHigh(guess);
+    }
 }
 
-function guessisCorrect(){
-
+function guessIsCorrect(guess){
+    console.log("correct");
+    const list = document.querySelector("#guess-list");
+    const html = `<li>You guessed ${guess} - That is correct! Congratulations!</li>`;
+    list.insertAdjacentHTML("beforeend", html);
 }
 
-function guessIsTooLow(){
-
+function guessIsTooLow(guess){
+    console.log("too low");
+    const list = document.querySelector("#guess-list");
+    const html = `<li>You guessed ${guess} - That was too low! Try again</li>`;
+    list.insertAdjacentHTML("beforeend", html);
 }
 
-function guessIsTooHigh(){
-
+function guessIsTooHigh(guess){
+    console.log("too high");
+    const list = document.querySelector("#guess-list");
+    const html = `<li>You guessed ${guess} - That was too high! Try again</li>`;
+    list.insertAdjacentHTML("beforeend", html);
 }
